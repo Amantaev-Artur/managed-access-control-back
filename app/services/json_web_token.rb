0196593d0dd979d71424dd095db061e715324d59
Amantaev-Artur <80975205+Amantaev-Artur@users.noreply.github.com>
2024-05-14 00:32:20 +0300

    Update json_web_token.rb

--- app/services/json_web_token.rb
+++ app/services/json_web_token.rb
@@ -3,9 +3,6 @@ require 'jwt'
 class JsonWebToken
   class << self
     def encode(payload)
-      p payload
-      p payload.class
-      p Rails.application.credentials.secret_key_base
       JWT.encode(payload, Rails.application.credentials.secret_key_base)
     end
 
